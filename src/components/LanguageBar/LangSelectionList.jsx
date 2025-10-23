5d02433878530af9eb47b3b27b832a00e1b26e34
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-10-23 05:39:05 +0300

    feat: add LangSelectionList component

---
+++ src/components/LanguageBar/LangSelectionList.jsx
@@ -0,0 +1,163 @@
+import useLangData from "../../store/useLangData";
+import { CloseIcon } from "../../ui/icons/index";
+import LanguageItem from "./LanguageItem";
+import { useState } from "react";
+
+const languagesList = [
+  { code: "af", name: "Afrikaans" },
+  { code: "sq", name: "Albanian" },
+  { code: "am", name: "Amharic" },
+  { code: "ar", name: "Arabic" },
+  { code: "hy", name: "Armenian" },
+  { code: "az", name: "Azerbaijani" },
+  { code: "eu", name: "Basque" },
+  { code: "be", name: "Belarusian" },
+  { code: "bn", name: "Bengali" },
+  { code: "bs", name: "Bosnian" },
+  { code: "bg", name: "Bulgarian" },
+  { code: "ca", name: "Catalan" },
+  { code: "ceb", name: "Cebuano" },
+  { code: "ny", name: "Chichewa" },
+  { code: "zh", name: "Chinese" },
+  { code: "co", name: "Corsican" },
+  { code: "hr", name: "Croatian" },
+  { code: "cs", name: "Czech" },
+  { code: "da", name: "Danish" },
+  { code: "nl", name: "Dutch" },
+  { code: "en", name: "English" },
+  { code: "eo", name: "Esperanto" },
+  { code: "et", name: "Estonian" },
+  { code: "tl", name: "Filipino" },
+  { code: "fi", name: "Finnish" },
+  { code: "fr", name: "French" },
+  { code: "fy", name: "Frisian" },
+  { code: "gl", name: "Galician" },
+  { code: "ka", name: "Georgian" },
+  { code: "de", name: "German" },
+  { code: "el", name: "Greek" },
+  { code: "gu", name: "Gujarati" },
+  { code: "ht", name: "Haitian" },
+  { code: "ha", name: "Hausa" },
+  { code: "haw", name: "Hawaiian" },
+  { code: "he", name: "Hebrew" },
+  { code: "hi", name: "Hindi" },
+  { code: "hmn", name: "Hmong" },
+  { code: "hu", name: "Hungarian" },
+  { code: "is", name: "Icelandic" },
+  { code: "ig", name: "Igbo" },
+  { code: "id", name: "Indonesian" },
+  { code: "ga", name: "Irish" },
+  { code: "it", name: "Italian" },
+  { code: "ja", name: "Japanese" },
+  { code: "jw", name: "Javanese" },
+  { code: "kn", name: "Kannada" },
+  { code: "kk", name: "Kazakh" },
+  { code: "km", name: "Khmer" },
+  { code: "rw", name: "Kinyarwanda" },
+  { code: "ko", name: "Korean" },
+  { code: "ku", name: "Kurdish" },
+  { code: "ky", name: "Kyrgyz" },
+  { code: "lo", name: "Lao" },
+  { code: "la", name: "Latin" },
+  { code: "lv", name: "Latvian" },
+  { code: "lt", name: "Lithuanian" },
+  { code: "lb", name: "Luxembourgish" },
+  { code: "mk", name: "Macedonian" },
+  { code: "mg", name: "Malagasy" },
+  { code: "ms", name: "Malay" },
+  { code: "ml", name: "Malayalam" },
+  { code: "mt", name: "Maltese" },
+  { code: "mi", name: "Maori" },
+  { code: "mr", name: "Marathi" },
+  { code: "mn", name: "Mongolian" },
+  { code: "my", name: "Myanmar" },
+  { code: "ne", name: "Nepali" },
+  { code: "no", name: "Norwegian" },
+  { code: "or", name: "Odia" },
+  { code: "ps", name: "Pashto" },
+  { code: "fa", name: "Persian" },
+  { code: "pl", name: "Polish" },
+  { code: "pt", name: "Portuguese" },
+  { code: "pa", name: "Punjabi" },
+  { code: "ro", name: "Romanian" },
+  { code: "ru", name: "Russian" },
+  { code: "sm", name: "Samoan" },
+  { code: "gd", name: "Scots" },
+  { code: "sr", name: "Serbian" },
+  { code: "st", name: "Sesotho" },
+  { code: "sn", name: "Shona" },
+  { code: "sd", name: "Sindhi" },
+  { code: "si", name: "Sinhala" },
+  { code: "sk", name: "Slovak" },
+  { code: "sl", name: "Slovenian" },
+  { code: "so", name: "Somali" },
+  { code: "es", name: "Spanish" },
+  { code: "su", name: "Sundanese" },
+  { code: "sw", name: "Swahili" },
+  { code: "sv", name: "Swedish" },
+  { code: "tg", name: "Tajik" },
+  { code: "ta", name: "Tamil" },
+  { code: "tt", name: "Tatar" },
+  { code: "te", name: "Telugu" },
+  { code: "th", name: "Thai" },
+  { code: "tr", name: "Turkish" },
+  { code: "tk", name: "Turkmen" },
+  { code: "uk", name: "Ukrainian" },
+  { code: "ur", name: "Urdu" },
+  { code: "ug", name: "Uyghur" },
+  { code: "uz", name: "Uzbek" },
+  { code: "vi", name: "Vietnamese" },
+  { code: "cy", name: "Welsh" },
+  { code: "xh", name: "Xhosa" },
+  { code: "yi", name: "Yiddish" },
+  { code: "yo", name: "Yoruba" },
+  { code: "zu", name: "Zulu" },
+];
+
+export default function LangSelectionList({ isInput, toggleList }) {
+  const [langList, setLangList] = useState(languagesList);
+  const whichBox = isInput ? "inputBox" : "outputBox";
+  const updateLangPairs = useLangData((state) => state.updateLangPairs);
+  const updateChoosedLang = useLangData((state) => state.updateChoosedLang);
+
+  const handleLangClick = (e) => {
+    const inputValue = e.target.value;
+    updateChoosedLang(whichBox, "name", e.target.labels[0].textContent);
+    updateChoosedLang(whichBox, "code", inputValue);
+    updateChoosedLang(whichBox, "checked", true);
+    updateLangPairs(whichBox, inputValue);
+    toggleList();
+  };
+
+  const handleSearch = (e) => {
+    const regPat = `^${e.target.value}`;
+    const reg = new RegExp(regPat, "i");
+    setLangList(() => languagesList.filter((lang) => lang.name.match(reg)));
+  };
+
+  return (
+    <article>
+      <header>
+        <input
+          type="text"
+          placeholder={isInput ? "Translate from" : "Translate to"}
+          onChange={handleSearch}
+        />
+        <button type="button" onClick={toggleList}>
+          <CloseIcon />
+        </button>
+      </header>
+      <section>
+        {langList.map((lang, idx) => (
+          <LanguageItem
+            key={idx}
+            langInfo={lang}
+            inputName={isInput ? "lang-pair-one" : "lang-pair-two"}
+            handleFn={handleLangClick}
+            role={"secondray"}
+          />
+        ))}
+      </section>
+    </article>
+  );
+}
