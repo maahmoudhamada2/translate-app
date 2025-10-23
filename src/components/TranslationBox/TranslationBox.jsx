5b2146ad754c49ab010c65242132b648ee0ae84b
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-10-23 07:04:54 +0300

    style(TranslationBox): add styling for TranslationBox component

--- src/components/TranslationBox/TranslationBox.jsx
+++ src/components/TranslationBox/TranslationBox.jsx
@@ -1,6 +1,7 @@
 import useLangData from "../../store/useLangData";
 import ActionButtons from "../ActionButtons/ActionButtons";
 import LanguageBar from "../LanguageBar/LanguageBar";
+import clsx from "clsx";
 
 export default function TranslationBox({ isInput }) {
   const whichBox = isInput ? "inputBox" : "outputBox";
@@ -8,9 +9,15 @@ export default function TranslationBox({ isInput }) {
   const content = useLangData((state) => state.langData[whichBox].sentence);
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form
+      className={clsx(
+        "w-full py-[2rem] px-[1rem] flex flex-col relative border-2 border-[#394150] rounded-3xl",
+        isInput ? "bg-[#212936CC]" : "bg-[#121826CC]"
+      )}
+      onSubmit={(e) => e.preventDefault()}>
       <LanguageBar isInput={isInput} />
       <textarea
+        className="w-full h-[150px] font-bold resize-none focus-visible:outline-0 p-[1rem]"
         id="translation-field"
         onChange={(e) => {
           const inputValue = e.target.value;
