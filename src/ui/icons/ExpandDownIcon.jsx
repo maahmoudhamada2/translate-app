70cc48aa659ec63cbdc42988ba29b215ca574a25
maahmoudhamada2 <maahmoud.hamada@hotmail.com>
2025-10-23 05:06:10 +0300

    feat(icons): add Logo & ExpandDown icons and create index.js file to manage exports

---
+++ src/ui/icons/ExpandDownIcon.jsx
@@ -0,0 +1,12 @@
+const ExpandDownIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    viewBox="0 0 16 16"
+    fill="none"
+    {...props}>
+    <path d="M12 6L8 10L4 6" stroke="#6C727F" strokeWidth={2} />
+  </svg>
+);
+export default ExpandDownIcon;
---
+++ src/ui/icons/index.js
@@ -0,0 +1,8 @@
+import ExpandDownIcon from "./ExpandDownIcon";
+import SoundIcon from "./SoundIcon";
+import CloseIcon from "./CloseIcon";
+import AlphaIcon from "./AlphaIcon";
+import CopyIcon from "./CopyIcon";
+import Logo from "./Logo";
+
+export { SoundIcon, Logo, ExpandDownIcon, CopyIcon, CloseIcon, AlphaIcon }
